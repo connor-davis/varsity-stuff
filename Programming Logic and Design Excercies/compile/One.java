36084d88af0344c4b64e0640b05c7eb34230bfcd
Connor Davis <cnnrproton@gmail.com>
2022-03-14 14:12:06 +0200

    Vat calculator

---
+++ Programming Logic and Design Excercies/compile/One.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class One {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            		double hrsPerDay;
+		double hrsInWorkWeek;
+		double hrsInWorkYear;
+		double DAYS_IN_WORK_WEEK = 5;
+		double DAYS_IN_WORK_YEAR = 252;
+	System.out.println("Please enter your hours you work every work day");
+	hrsPerDay = Double.parseDouble(scanner.next());
+	hrsInWorkWeek = hrsPerDay * DAYS_IN_WORK_WEEK;
+	hrsInWorkYear = hrsPerDay * DAYS_IN_WORK_YEAR;
+	System.out.println("You work " +  hrsInWorkWeek +  " hours in a work week");
+	System.out.println("You work " +  hrsInWorkYear +  " hours in a work year");
+
+        }
+    }
+}
