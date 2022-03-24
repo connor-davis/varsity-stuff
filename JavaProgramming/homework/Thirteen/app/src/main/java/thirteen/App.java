f302d31efcc9069968f18e1153148c44d2396b4c
Connor Davis <cnnrproton@gmail.com>
2022-03-24 13:09:47 +0200

    Question Thirteen for Homework

---
+++ JavaProgramming/homework/Thirteen/app/src/main/java/thirteen/App.java
@@ -0,0 +1,14 @@
+package thirteen;
+
+/**
+ * Connor Davis
+ * @author ST10068305
+ */
+public class App {
+    /**
+     * @param args The command line arguments
+     */
+    public static void main(String[] args) {
+       
+    }
+}
