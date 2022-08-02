05809d5dc51d1e83a6a0f675bdf461139741b65c
Connor Davis <cnnrproton@gmail.com>
2022-08-02 13:13:12 +0200

    Inheritance Basics

---
+++ JavaProgramming/InheritBasics/inheritbasics/src/main/java/za/edu/vcconnect/st10068305/App.java
@@ -0,0 +1,13 @@
+package za.edu.vcconnect.st10068305;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
